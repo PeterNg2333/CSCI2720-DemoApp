cf7c9414e92acc7f84dfee1c1027360d6d07ee10
NgKinPak <95013548+PeterNg2333@users.noreply.github.com>
2022-12-12 20:26:59 +0800

    update

--- frontend/src/pages/UserNavBar.js
+++ frontend/src/pages/UserNavBar.js
@@ -5,38 +5,40 @@ class UserNavBar extends React.Component{
     render(){
         return(
             <nav className="container-fluid navbar navbar-expand nav-user">
+                <div>
+                    <button className="navbar-toggle btn">
+                        <i className="fa fa-chevron-left text-white"></i>
+                    </button>
+                    <button className="navbar-toggle btn">
+                        <i className="fa fa-chevron-right text-white"></i>
+                    </button>
+                </div>
 
-            <div>
-                <button className="navbar-toggle btn">
-                    <i className="fa fa-chevron-left text-white"></i>
+                <ul className="nav navbar-nav ms-auto">
+                    <li className="mx-1 nav-item ">
+                        <Link to="/" className="nav-item-text">
+                            <button className="btn navbar-btn nav-item-text text-white">
+                                <i className="fa fa-light fa-home mr-2"> Home </i>
+                            </button>
+                        </Link>
+                    </li>
+                    <li className="mx-1 nav-item">
+                        <Link to="main" className="nav-item-text">
+                            <button className="btn navbar-btn nav-item-text text-white">
+                                <i className="fa fa-regular fa-heart mr-2"> Favorite </i>
+                            </button>
+                        </Link>
+                    </li>
+                </ul>
+
+                <button className="btn navbar-btn nav-item-text text-white no-click" >
+                    <i className="fa fa-light fa-user mr-1"> Hi!, [User Name] </i>
                 </button>
-                <button className="navbar-toggle btn">
-                    <i className="fa fa-chevron-right text-white"></i>
+                
+                <button className="btn navbar-btn nav-item-text text-white" >
+                    Log Out<i className="fa fa-thin fa-sign-out mx-1"> </i>
                 </button>
-            </div>
-            <ul className="nav navbar-nav ms-auto">
-                <li className="mx-1 nav-item ">
-                    <Link to="/" className="nav-item-text">
-                        <button className="btn navbar-btn nav-item-text text-white">
-                            <i className="fa fa-light fa-home mr-2"> Home </i>
-                        </button>
-                    </Link>
-                </li>
-                <li className="mx-1 nav-item">
-                    <Link to="main" className="nav-item-text">
-                        <button className="btn navbar-btn nav-item-text text-white">
-                            <i className="fa fa-regular fa-heart mr-2"> Favourite locations </i>
-                        </button>
-                    </Link>
-                </li>
-            </ul>
-            <button className="btn navbar-btn nav-item-text text-white">
-                <i className="fa fa-light fa-user mr-1"> Hi!, [User Name]] </i>
-            </button>
-            <button className="btn navbar-btn nav-item-text text-white">
-                Log Out<i className="fa fa-thin fa-sign-out mx-1"> </i>
-            </button>
-        </nav>
+            </nav>
         );
     }
 }
