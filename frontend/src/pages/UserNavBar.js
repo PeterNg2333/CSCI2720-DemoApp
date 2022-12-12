c7b86a36478f50a6ab80bbd8696e6bf43801fdfc
NgKinPak <95013548+PeterNg2333@users.noreply.github.com>
2022-12-12 19:13:12 +0800

    update

--- frontend/src/pages/UserNavBar.js
+++ frontend/src/pages/UserNavBar.js
@@ -4,7 +4,7 @@ import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 class UserNavBar extends React.Component{
     render(){
         return(
-            <nav className="navbar navbar-expand bg-primary">
+            <nav className="container-fluid navbar navbar-expand nav-user">
             <div>
                 <button className="navbar-toggle btn">
                     <i className="fa fa-chevron-left text-white"></i>
@@ -14,11 +14,27 @@ class UserNavBar extends React.Component{
                 </button>
             </div>
             <ul className="nav navbar-nav ms-auto">
-                <li className="mx-4 nav-item"><Link to="/" className="nav-item-text">Home</Link></li>
-                <li className="mx-4 nav-item"><Link to="main" className="nav-item-text">Favourite locations</Link></li>
-                <li className="mx-4 nav-item"><Link to="/" className="nav-item-text">User Name</Link></li>
+                <li className="mx-1 nav-item ">
+                    <Link to="/" className="nav-item-text">
+                        <button className="btn navbar-btn nav-item-text text-white">
+                            <i className="fa fa-light fa-home mr-2"> Home </i>
+                        </button>
+                    </Link>
+                </li>
+                <li className="mx-1 nav-item">
+                    <Link to="main" className="nav-item-text">
+                        <button className="btn navbar-btn nav-item-text text-white">
+                            <i className="fa fa-regular fa-heart mr-2"> Favourite locations </i>
+                        </button>
+                    </Link>
+                </li>
             </ul>
-            <button className="btn navbar-btn nav-item-text">Log Out</button>
+            <button className="btn navbar-btn nav-item-text text-white">
+                <i className="fa fa-light fa-user mr-1"> Hi!,[[User Name]]] </i>
+            </button>
+            <button className="btn navbar-btn nav-item-text text-white">
+                Log Out<i className="fa fa-thin fa-sign-out mx-1"> </i>
+            </button>
         </nav>
         );
     }
