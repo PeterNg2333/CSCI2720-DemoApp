40de4b5ed42ec9a1fe5550945f3c5ab6e959be9c
NgKinPak <95013548+PeterNg2333@users.noreply.github.com>
2022-12-12 15:59:07 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,30 @@ import AdminPanelEventPage from "./pages/AdminPanelEventPage";
 
 function App() {
     return (
-        <BrowserRouter>
-            <header>
-                <nav className="navbar navbar-expand bg-primary">
-                    <div>
-                        <button className="navbar-toggle btn">
-                            <i className="fa fa-chevron-left text-white"></i>
-                        </button>
-                        <button className="navbar-toggle btn">
-                            <i className="fa fa-chevron-right text-white"></i>
-                        </button>
-                    </div>
-                    <ul className="nav navbar-nav ms-auto">
-                        <li className="mx-4 nav-item"><Link to="/" className="nav-item-text">Home</Link></li>
-                        <li className="mx-4 nav-item"><Link to="/" className="nav-item-text">Favourite locations</Link></li>
-                        <li className="mx-4 nav-item"><Link to="/" className="nav-item-text">User Name</Link></li>
-                    </ul>
-                    <button className="btn navbar-btn nav-item-text">Log Out</button>
-                </nav>
-            </header>
-            <Routes>
-                <Route path="/" element={<AdminPanelEventPage/>}/>
-                {/*<Route path="*" element={<NoMatch/>}/>*/}
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+      <header>
+      <nav className="navbar navbar-expand bg-primary">
+              <div>
+                  <button className="navbar-toggle btn">
+                      <i className="fa fa-chevron-left text-white"></i>
+                  </button>
+                  <button className="navbar-toggle btn">
+                      <i className="fa fa-chevron-right text-white"></i>
+                  </button>
+              </div>
+              <ul className="nav navbar-nav ms-auto">
+                  <li className="mx-4 nav-item"><Link to="/" className="nav-item-text">Home</Link></li>
+                  <li className="mx-4 nav-item"><Link to="main" className="nav-item-text">Favourite locations</Link></li>
+                  <li className="mx-4 nav-item"><Link to="/" className="nav-item-text">User Name</Link></li>
+              </ul>
+              <button className="btn navbar-btn nav-item-text">Log Out</button>
+          </nav>
+      </header>
+      <Routes>
+          <Route path="/" element={<AdminPanelEventPage/>}/>
+          {/*<Route path="*" element={<NoMatch/>}/>*/}
+      </Routes>
+  </BrowserRouter>
     );
 }
 
