ac4d71f17a78b7457fa2bf2cfd54c30b8296cfe4
NgKinPak <95013548+PeterNg2333@users.noreply.github.com>
2022-12-12 22:27:29 +0800

    change

--- frontend/src/pages/UserLocationMainPage.js
+++ frontend/src/pages/UserLocationMainPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 class UserLocationMain extends React.Component{
     render(){
         return(
-            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+            <div className="container-fluid">
                 
             </div>
         );
