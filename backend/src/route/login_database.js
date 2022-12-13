5177519c49672a3e2f838c4c3e58aa1ba6c511f6
Allen <>
2022-12-14 00:13:48 +0800

    Add: CRUD of user database and logout button

--- backend/src/route/login_database.js
+++ backend/src/route/login_database.js
@@ -4,16 +4,27 @@
 //So we made the Creatnew user here to test.
 
 // mongodb connection
+const express = require("express");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
+
 const {userSchema} = require("../service/database/schema");
-const bcrypt = require('bcryptjs');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+
 
 const DATABASE_CONNECT_STRING = "mongodb://localhost:27017/csci2720project";
 mongoose.connect(DATABASE_CONNECT_STRING);
 
 const User = mongoose.model("User", userSchema);
 
-async function checkIn(req, res) {
+
+
+async function login(req, res) {
     User.findOne({username: req.body['username']}, (err, e) => {
         if (err)
           res.send(err);       
@@ -27,7 +38,8 @@ async function checkIn(req, res) {
                 bcrypt.compare(req.body['password'], e.password, function(error, isMatch) {
                     if (error) {
                     } else if(isMatch) {
-                      res.redirect('/adminpage');
+                      res.redirect('/admin');
+                      //res.cookie("username", e.username);
                     } else{
                         res.set('Content-Type', 'text/plain');
                         res.status(404).send("Wrong password.");
@@ -37,10 +49,13 @@ async function checkIn(req, res) {
                 bcrypt.compare(req.body['password'], e.password, function(error, isMatch) {
                     if (error) {
                     } else if (isMatch) {
-                        res.redirect('/mainpage');
+                        //res.cookie("username", e.username);
+                        res.redirect('/main');
                     }else{
                         res.set('Content-Type', 'text/plain');
                         res.status(404).send("Wrong password.");
+                        res.redirect('/login');
+
                     }
                 });
             }       
@@ -69,16 +84,105 @@ async function createUser(req, res) {
                 else{
                     res.status(201);
                     res.set('Content-Type', 'text/plain');
-                    res.send(e);
+                    res.send(JSON.stringify(e, null, ' '));
                 }        
             });
         }
     });
 }
 
+async function getUser(req, res) {
+    User.findOne({username: req.body['username']}, (err, e) => {
+        //console.log(req.body['username']);
+        if (!e){
+            res.status(404);
+            res.set('Content-Type', 'text/plain');
+            res.send("No User Found");
+        }
+        else{
+            res.send(JSON.stringify(e, null, ' '));
+        }
+    })
+}
 
+
+async function updateUser(req, res) {
+
+    User.findOne({ userId: req.body['userId']}, (err, e) => {
+        if (err)
+            res.send(err);
+        else {
+            e.username = req.body['username'];
+            e.password = req.body['password'];
+            e.save();
+            res.set('Content-Type', 'text/plain');
+            res.send(JSON.stringify(e, null, ' '));
+        }
+    });
+
+}
+
+async function deleteUser(req, res) {
+    User.findOne({userId: req.body['userId']}, (err, e) => {
+        if (err)
+          res.send(err);       
+        else {
+          if (e == null){
+              res.set('Content-Type', 'text/plain');
+              res.status(404).send("User is not found.");
+          } else {
+              User.deleteOne({ userId: e.userId }, (err, result) => {
+                  if (err)
+                      res.send(err);
+                  else {
+                      res.set('Content-Type', 'text/plain');
+                      res.status(204).send();
+                  }
+              });
+          }
+        }
+      });
+}
+
+async function getAllUser(req, res) {
+    User.find({}, (err_e, e) => {
+        if (err_e)
+            res.send(err_e);
+        else {
+            if (e == null) {
+                res.status(404);
+                res.set('Content-Type', 'text/plain');
+                res.send("There is no user.");
+            }
+            else {
+                let all = [];
+                for (let i = 0; i < e.length; i++) {
+                    all.push({ UserId: e[i].userId, username: e[i].username, password: e[i].password });
+                }
+                res.set('Content-Type', 'text/plain');
+                res.send(JSON.stringify(all, null, ' '));
+            }
+        }
+    });
+}
+
+/*
+async function alll(req, res) {
+    if (req.cookies['visited'] === undefined) {
+        res.cookie('visited', 'yes', { maxAge: '1200000'});
+        res.send('Your first visit!');
+        } else {
+        res.send('Welcome back!');
+        }
+}
+*/
 
 module.exports = {
-    checkIn,
-    createUser
+    login,
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser,
+    getAllUser,
+
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -7,23 +7,47 @@ const {
     addCommentByVenueRoute,
 } = require("./route/database");
 
-const { checkIn, createUser} = require("./route/login_database");
+const { login, createUser, getUser, updateUser, deleteUser, getAllUser} = require("./route/login_database");
 
 function mountRouter(app) {
     
     //app.get("/", (req, res) => res.send("Hello world"));
+    //html file for debug
     app.get("/login", (req, res) => res.sendFile(__dirname + '/loginpage.html'));
-    app.get("/mainpage", (req, res) => res.sendFile(__dirname + '/mainpage.html'));
-    app.get("/adminpage", (req, res) => res.sendFile(__dirname + '/adminpage.html'));
-    app.post('/login', checkIn);
-    app.post('/login/creat', createUser);
+    app.get("/main", (req, res) => res.sendFile(__dirname + '/mainpage.html'));
+    app.get("/admin", (req, res) => res.sendFile(__dirname + '/adminpage.html'));
+    //
+
+
+    //login
+    app.post('/login', login);
+
+    //CRUD user
+    app.post('/admin/createuser', createUser);
+    app.post('/admin/getuser', getUser);
+    app.put('/admin/updateuser', updateUser);
+    app.delete('/admin/deleteuser', deleteUser);
+
+    app.get('/admin/getalluser', getAllUser);
+
+    //logout
+    app.post('/logout', (req, res) => {
+
+      res.redirect('/login');
+
+    });
+
+
     app.get("/venue/all", getAllVenuesRoute);
     app.get("/venue/all/keywords", getVenuesByKeywordRoute);
     app.post("/venue/add/favourite", addVenueToUserFavouriteRoute);
     app.get("/comment/get/venue", getCommentsByVenueRoute);
     app.post("/comment/add/venue", addCommentByVenueRoute);
     
-    app.all("/*", (req, res) => res.redirect('/login'));
+    app.all("/*", (req, res) => {
+        
+        res.redirect('/login');
+    });
 }
 
 
