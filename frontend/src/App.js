0820cb50736155b03b87919c9a4e73a0dc4223c4
brandonwsc <brandon@Brandons-MacBook-Pro.local>
2022-12-13 18:02:26 +0800

    Optimized the code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './css/userPage.css';
 import "./App.css";
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 import AdminPanelEventPage from "./pages/AdminPanelEventPage";
-import UserNavBar from "./pages/UserNavBar";
+import UserNavBar from "./components/UserNavBar";
 import UserLocationMain from "./pages/UserLocationMainPage"
 
 function App() {
