f7bca89b17cce650692ff6504281b7d640e12a8a
Carsoncyc <carson0517cyc@gmail.com>
2022-12-14 20:56:42 +0800

    fixed router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -24,7 +24,7 @@ const {
 } = require("./route/login_database");
 
 function mountRouter(app) {
-  app.get("/", (req, res) => res.send("Hello world"));
+  //app.get("/", (req, res) => res.send("Hello world"));
   // <<<<<<< HEAD
   app.get("/login", (req, res) => res.sendFile(__dirname + "/loginpage.html"));
   app.get("/mainpage", (req, res) =>
@@ -33,8 +33,7 @@ function mountRouter(app) {
   app.get("/adminpage", (req, res) =>
     res.sendFile(__dirname + "/adminpage.html")
   );
-  app.post("/login", checkIn);
-  app.post("/login/creat", createUser);
+  app.post("/login", login);
 
   //   CRUD user
   app.post("/admin/createuser", createUser);
@@ -59,35 +58,7 @@ function mountRouter(app) {
   app.post("/venue/comment/create", addCommentByVenueRoute);
 
   app.all("/*", (req, res) => res.redirect("/login"));
-  // =======
-  //html file for debug
-  //   app.get("/login", (req, res) => res.sendFile(__dirname + "/loginpage.html"));
-  //   app.get("/main", (req, res) => res.sendFile(__dirname + "/mainpage.html"));
-  //   app.get("/admin", (req, res) => res.sendFile(__dirname + "/adminpage.html"));
-  //   //
-  //   //login
-  //   app.post("/login", login);
-  //   //CRUD user
-  //   app.post("/admin/createuser", createUser);
-  //   app.post("/admin/getuser", getUser);
-  //   app.put("/admin/updateuser", updateUser);
-  //   app.delete("/admin/deleteuser", deleteUser);
-  //   app.get("/admin/getalluser", getAllUser);
-  //   //logout
-  //   app.post("/logout", (req, res) => {
-  //     res.redirect("/login");
-  //   });
-  //   app.get("/venue/all", getAllVenuesRoute);
-  //   app.get("/venue/all/keywords", getVenuesByKeywordRoute);
-  //   app.post("/venue/add/favourite", addVenueToUserFavouriteRoute);
-  //   app.get("/comment/get/venue", getCommentsByVenueRoute);
-  //   app.post("/comment/add/venue", addCommentByVenueRoute);
 
-  //   app.all("/*", (req, res) => {
-  //     res.redirect("/login");
-  //   });
-
-  // >>>>>>> login-Carson-Allen-2
 }
 
 module.exports = { mountRouter };
