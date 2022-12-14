24365aef04e5ca31756717ebd593ee15d7f42170
NgKinPak <95013548+PeterNg2333@users.noreply.github.com>
2022-12-14 23:21:42 +0800

    LogIn

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 
-// LHS of the login Pages in the protoype
-function LoginImage(){
+function LoginPage(){
     return(
+        <main className='container-lg'>
+            <div className='row'>
+                <LoginPicture/>
+                <LoginForm/>
+            </div>
+        </main>
 
+    );
+}
+
+// LHS of the login Pages in the protoype
+function LoginPicture(){
+    return(
+        <section className='col-6 logInPictureBg'
+            style={{backgroundImage: 'url("./Resource/LoginPage/WebLogInImage.png")'}}>
+            HI
+        </section>
     );
 }
 
 // RHS of the login Pages in the protoype
 function LoginForm(){
     return(
-        
+        <section className='col-6 logInFormBg'>HI</section>
     );
 }
+
+export default LoginPage;
