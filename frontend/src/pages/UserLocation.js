e25b4e65925978765441bec756ab11d7301f922a
NgKinPak <95013548+PeterNg2333@users.noreply.github.com>
2022-12-14 10:44:09 +0800

    location done

--- frontend/src/pages/UserLocation.js
+++ frontend/src/pages/UserLocation.js
@@ -44,12 +44,16 @@ function MapSection(){
 function VenueSection(){
     return(
         <section className='col-lg-6 '>
-            <div className='container-fluid align-middle bg-light mt-4 pb-4  table-responsive'>
+            <div className='container-fluid bg-light mt-4 pb-4  table-responsive'>
                 <table className="col-5 table mt-3 table-hover">
                     <thead className='shadow shadow-sm border-bottom-1'>
-                        <tr className=''>
-                        <th scope="col" colspan="2" className='border border-dark'>Location</th>
-                        <th scope="col" className='border border-dark '> What's on </th>
+                        <tr className='' style={{textAlign: "center"}}>
+                            <th scope="col" colspan="2" className='border border-dark '>
+                                <h3 ><strong>Location </strong></h3>
+                            </th>
+                            <th scope="col" className='border border-dark '>
+                                <h3><strong>What's on</strong></h3>
+                            </th>
                         </tr>
                     </thead >
 
--- frontend/src/pages/UserVenueFileCard.js
+++ frontend/src/pages/UserVenueFileCard.js
@@ -4,11 +4,42 @@ import placeholder_canRemove from './placeholder_canRemove.png'
 function UserVenueFileCard(){
     return (
         <tr className=''>
-            <th scope="row">1ssssssssssssssssssssssssssssss</th>
-            <td>
-            <img className='img-fluid'  alt="img" style={{display: "inline", maxWidth: '100%', verticalAlign:"middle", height: 'auto'}}/>
+            <th scope="row">
+                <div className='row'>
+                    <h4 className='col-7' style={{color: "#181E84"}}>Location A</h4>
+                    
+                    <form  class="custom-checkbox d-inlineBlock col-4 mt-1">
+                    <label for={/**assign a unique id*/ "LikeButton"}  id="likeButtionLabel">
+                        <input type="checkbox" id={/**assign a unique id*/ "LikeButton"} className='label' style={{}}/>
+                        <i className="fa fa-star-o"></i>
+                        <i className="fa fa-star"></i>
+                        <span>Like</span>
+                    </label>
+                    </form>
+                </div>
+
+                <div className='row'>
+                    <p className='mx-3'>
+                        <i className='fa fa-map'></i> This is a address
+                    </p>
+
+                    <p className='mx-3'>
+                        <i className='fa fa-info'></i> This is some information about the location
+                    </p>
+                </div>
+               
+            </th>
+
+            <td width={100}>
+                <div>
+                <img className='img-fluid' src={placeholder_canRemove} alt="img" style={{ maxWidth: '100px', verticalAlign:"middle", height: 'auto'}}/>
+                </div>
+        
             </td>
-            <td>Otto</td>
+
+            <td className=''>
+
+            </td>
         </tr>
     );
 }
