eb4a00950822741f39e0d3e06a67a907927c7efd
brandonwsc <brandon@Brandons-MacBook-Pro.local>
2022-12-16 03:44:04 +0800

    Removed unused css.

--- frontend/src/pages/AdminPanelEventPage.js
+++ frontend/src/pages/AdminPanelEventPage.js
@@ -10,16 +10,16 @@ function AdminPanelEventPage() {
             <AdminNavBar isAdmin/>
             <form className='mt-3 d-flex flex-row '>
                 <section className="col mx-2">
-                    <input type="search" className="form-control border border-dark" id="location" placeholder="Location"/>
+                    <input type="search" className="form-control border border-dark h-100" id="location" placeholder="Location"/>
                 </section>
                 <section className="col mx-2">
-                    <input type="search" className="form-control border border-dark" id="eventId" placeholder="Event ID"/>
+                    <input type="search" className="form-control border border-dark h-100" id="eventId" placeholder="Event ID"/>
                 </section>
                 <section className="col mx-2">
-                    <input type="search" className="form-control border border-dark" id="eventName" placeholder="Event Name"/>
+                    <input type="search" className="form-control border border-dark h-100" id="eventName" placeholder="Event Name"/>
                 </section>
                 <section className="col mx-2">
-                    <select className="form-select border border-dark" aria-label="Even Type">
+                    <select className="form-select border border-dark h-100" aria-label="Even Type">
                         <option selected>Event Type</option>
                         {
                             eventTypes.map((eventType,index)=>{
@@ -29,13 +29,13 @@ function AdminPanelEventPage() {
                     </select>
                 </section>
                 <section className="col mx-2">
-                    <input type="search" className="form-control border border-dark" id="eventName" placeholder="Date and time"/>
+                    <input type="search" className="form-control border border-dark h-100" id="eventName" placeholder="Date and time"/>
                 </section>
                 <section className="col mx-2">
-                    <button type="button" className="btn btn-sm btn-dark one-line bg-deep-blue">Find Event</button>
+                    <button type="button" className="btn btn-sm btn-dark one-line bg-deep-blue h-100 w-100">Find Event</button>
                 </section>
                 <section className="col mx-2">
-                    <select className="form-select border border-dark" aria-label="Order by">
+                    <select className="form-select border border-dark h-100" aria-label="Order by">
                         <option selected>Order by</option>
                         <option value="location">Location</option>
                         <option value="eventId">Event ID</option>
@@ -44,13 +44,13 @@ function AdminPanelEventPage() {
                     </select>
                 </section>
                 <section className="col mx-2">
-                <button type="button" className="btn btn-outline-light bg-transparent d-flex align-content-center justify-content-around">
+                <button type="button" className="btn btn-outline-light bg-transparent d-flex align-content-center justify-content-around h-100">
                     <i className="fa fa-circle-plus color-green fa-2x"></i>
                     <div className='color-green'>Create</div>
                 </button>
                 </section>
                 <section className="col mx-2 ">
-                    <button type="button" className="btn btn-outline-light bg-transparent d-flex align-content-center justify-content-around">
+                    <button type="button" className="btn btn-outline-light bg-transparent d-flex align-content-center justify-content-around h-100">
                         <i className="fas fa-trash-alt color-red fa-2x" ></i>
                         <div className='color-red'>Delete</div>
                     </button>
