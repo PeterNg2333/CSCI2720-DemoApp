9e4f57cd58b15839822fbfb2bcad318f2848b040
Janice Tong <team@originbit.asia>
2022-12-17 13:40:08 +0800

    comment api debug

--- backend/src/service/database/schema.js
+++ backend/src/service/database/schema.js
@@ -1,37 +1,36 @@
-const {Schema} = require("mongoose");
-const bcrypt = require('bcryptjs'),
-SALT_WORK_FACTOR = 10;
+const { Schema } = require("mongoose");
+const bcrypt = require("bcryptjs"),
+  SALT_WORK_FACTOR = 10;
 const mongoose = require("mongoose");
-
 
 // updated by Carson&Allen 2022-12-11 yymmdd
 const userSchema = new Schema({
-    userId: { type: Number, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: {type: Boolean, required: true},
-    favouriteVenues: [Number],
+  userId: { type: Number, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true },
+  favouriteVenues: [Number],
 });
 
 // hash the password
-userSchema.pre('save', function(next) {
-    var user = this;
+userSchema.pre("save", function (next) {
+  var user = this;
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
 
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
     });
+  });
 });
 
 /*
@@ -57,38 +56,38 @@ userSchema.pre('updateOne', async function() {
 });
 */
 // hash the password
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
 const commentSchema = new Schema({
-    id: Number,
-    userId: Number,
-    venue: Number,
-    text: String,
+  id: Number,
+  userId: Number,
+  venueId: Number,
+  text: String,
 });
 
 const venueSchema = new Schema({
-    id: Number,
-    name: String,
-    latitude: Number,
-    longitude: Number,
+  id: Number,
+  name: String,
+  latitude: Number,
+  longitude: Number,
 });
 
 const eventSchema = new Schema({
-    eventId: Number,
-    title: String,
-    venueId: Number,
-    datetime: String,
-    description: String,
-    presenter: String,
-    price: String,
-    programTime: String,
-    ageLimit: String,
-    remark: String,
+  eventId: Number,
+  title: String,
+  venueId: Number,
+  datetime: String,
+  description: String,
+  presenter: String,
+  price: String,
+  programTime: String,
+  ageLimit: String,
+  remark: String,
 });
 
-module.exports = {userSchema, commentSchema, venueSchema, eventSchema,};
+module.exports = { userSchema, commentSchema, venueSchema, eventSchema };
